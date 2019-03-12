df7b13bfadd075eb026bcd283cb12496732b4cfc
acont42 <48134509+acont42@users.noreply.github.com>
2019-03-12 10:33:30 -0700

    added line of code to DisplayEvents execute()

--- CRMProject/src/features/consoleView/DisplayEvents.java
+++ CRMProject/src/features/consoleView/DisplayEvents.java
@@ -18,6 +18,7 @@ public class DisplayEvents implements MenuItem {
     
      public void execute(){
         // DISPLAY EVENTS WILL CYCLE THROUGH DAYS OF WEEK, CYCLE THROUGH EACH DAY'S EVENTLIST AND DISPLAY EVENT INFO 
+        System.out.println("Displaying this week's events...");
         for (Day d : thisWeek.getCurrentWeek()) {
             for (Event e : d.getDaysEvents().getEventList()) {
                 System.out.println("Event: " + e.getEventName());
