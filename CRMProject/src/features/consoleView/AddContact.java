078395f8ed996883fb95a649b806e10aa8a1f26d
abspetersen <48136261+abspetersen@users.noreply.github.com>
2019-03-12 13:57:49 -0700

    Update AddContact.java

--- CRMProject/src/features/consoleView/AddContact.java
+++ CRMProject/src/features/consoleView/AddContact.java
@@ -13,8 +13,21 @@ import consoleKit.MenuItem;
  */
 public class AddContact implements MenuItem{
     
+       private ContactList contacts;
+    
+    public AddContact(ContactList cons){
+        contacts = cons;
+    }
+    
     public void execute(){
-        
+        System.out.print("Enter the name of the new contact: ");
+        String name = STDIN.next();
+        System.out.print("Enter the phone number of the new contact: ");
+        int pnum = STDIN.nextInt();
+        System.out.print("Enter the email of the new contact: ");
+        String email = STDIN.next();
+        Contact c = new Contact(name, pnum, email);
+        contacts.add(c);
     }
     
     // Description of this command
